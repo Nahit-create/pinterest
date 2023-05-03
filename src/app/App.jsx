402c1313fa6e591c530b7b147a807613005342bd
Nahit-create <iftichan@gmail.coom>
2023-05-03 16:16:39 +0530

    first

---
+++ src/app/App.jsx
@@ -0,0 +1,25 @@
+import MobileNav from 'components/MobileNav';
+import Navbar from 'components/Navbar';
+import Home from 'pages/Home';
+import Today from 'pages/Today';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Router basename='#'>
+        <div className='app'>
+          <MobileNav />
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/today' component={Today} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
